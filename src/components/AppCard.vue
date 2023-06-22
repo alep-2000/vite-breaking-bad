1a2a0fefdd204a498e86d58167e988af5139f212
alep-2000 <alessiopiras056@gmail.com>
2023-06-22 20:26:28 +0200

    Exercise finisched

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -9,14 +9,31 @@ export default {
 </script>
 
 <template>
-    <div class="d-flex justify-content-center align-items-center mt-3">
-        <img :src="myPokemons.imageUrl">
-    </div>
-    <div class="text-center">
-        <div> {{ myPokemons.code }} </div>
-        <div> <strong>{{ myPokemons.name }} </strong></div>
-        <div> {{ myPokemons.type1 }} </div>
+    <div :class="myPokemons.type1 === 'Grass' ? 'claire' : myPokemons.type1 === 'Fire' ? 'red' : myPokemons.type1 === 'Water' ? 'blue' : ''"
+        class="card Kard my-2">
+        <div class="d-flex justify-content-center align-items-center mt-3">
+            <img :src="myPokemons.imageUrl">
+        </div>
+        <div class="text-center">
+            <div> {{ myPokemons.code }} </div>
+            <div> <strong>{{ myPokemons.name }} </strong></div>
+            <div class="mb-3"> {{ myPokemons.type1 }} </div>
+        </div>
     </div>
 </template>
 
-<style lang="scss"></style>
+<style lang="scss">
+.Kard {
+    height: 240px;
+
+
+    img {
+        width: 120px;
+        height: 120px;
+        border-radius: 50%;
+        padding: 5px;
+        object-fit: cover;
+    }
+
+}
+</style>
--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -15,10 +15,10 @@ export default {
 </script>
 
 <template>
-    <div class="container-md background-grey d-flex align-items-center">
+    <div class="container background-grey rounded-4 p-4 d-flex align-items-center">
         <div class="container-sm sub-container">
             <div class="row">
-                <div class="col-6 col-md-2 card d-flex flex-wrap " v-for="(pokemon, index) in store.pokemons" :key="index">
+                <div class=" col-6 col-md-4 col-lg-3" v-for="(pokemon, index) in store.pokemons  " :key="index">
                     <AppCard :myPokemons="pokemon" />
                 </div>
             </div>
@@ -26,32 +26,28 @@ export default {
     </div>
 </template>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .background-grey {
-    width: 100%;
-    height: 85vh;
     background-color: #DEDEDE;
-    border-radius: 10px;
 
     .sub-container {
         width: 100%;
-        height: 75vh;
+        height: 45vh;
         overflow-y: scroll;
         background-color: rgb(84, 89, 94);
         background: linear-gradient(90deg, rgba(84, 89, 94, 1) 54%, rgba(125, 129, 133, 1) 100%);
 
-        .card {
-            height: 240px;
-            margin: 20px;
 
-            img {
-                width: 120px;
-                height: 120px;
-                border-radius: 50%;
-                padding: 5px;
-                object-fit: cover;
-                background-color: white;
-            }
+        .claire {
+            background-color: green;
+        }
+
+        .red {
+            background-color: red;
+        }
+
+        .blue {
+            background-color: blue;
         }
     }
 
