e82b4bab56b3ee83db00d78370e5872521b1f252
alep-2000 <alessiopiras056@gmail.com>
2023-06-27 17:43:32 +0200

    [L]Add computed for color background card

--- src/components/AppCard.vue
+++ src/components/AppCard.vue
@@ -1,30 +1,12 @@
 <script>
 import { store } from '../components/data/store';
+import { colorMap } from '../components/data/index';
 
 export default {
     data() {
         return {
             store,
-            colorMap: {
-                "Bug": 'rgb(163, 231, 99)',
-                "Dark": 'rgb(77, 77, 77)',
-                "Dragon": 'rgb(117,93,225)',
-                "Electric": 'rgb(255, 255, 0)',
-                "Fairy": 'rgb(255, 192, 203)',
-                "Fighting": 'rgb(158, 76, 76)',
-                "Fire": 'rgb(255, 0, 0)',
-                "Flying": 'rgb(173, 216, 230)',
-                "Ghost": 'rgb(100, 0, 150)',
-                "Grass": 'rgb(76, 158, 76)',
-                "Ground": 'rgb(158, 123, 9)',
-                "Ice": 'rgb(106, 186, 212)',
-                "Normal": 'rgb(190, 190, 190)',
-                "Poison": 'rgb(128, 0, 128)',
-                "Psychic": 'rgb(201, 0, 184)',
-                "Rock": 'rgb(100, 50, 50)',
-                "Steel": 'rgb(148, 148, 148)',
-                "Water": 'rgb(0, 0, 255)'
-            }
+            colorMap
         }
     },
     props: {
@@ -34,6 +16,9 @@ export default {
     computed: {
         upperName() {
             return this.myPokemons.name.toUpperCase();
+        },
+        backColor() {
+            return colorMap[store.mainType];
         }
     }
 }
@@ -41,7 +26,8 @@ export default {
 
 <template>
     <div class="card my-2"
-        :class="myPokemons.type1 === 'Grass' ? 'grass_back' : myPokemons.type1 === 'Fire' ? 'fire_back' : myPokemons.type1 === 'Water' ? 'water_back' : myPokemons.type1 === 'Bug' ? 'bug_back' : myPokemons.type1 === 'Dark' ? 'dark_back' : myPokemons.type1 === 'Dragon' ? 'dragon_back' : myPokemons.type1 === 'Electric' ? 'electric_back' : myPokemons.type1 === 'Fairy' ? 'fairy_back' : myPokemons.type1 === 'Fighting' ? 'fighting_back' : myPokemons.type1 === 'Flying' ? 'flying_back' : myPokemons.type1 === 'Ghost' ? 'ghost_back' : myPokemons.type1 === 'Ground' ? 'ground_back' : myPokemons.type1 === 'Ice' ? 'ice_back' : myPokemons.type1 === 'Normal' ? 'normal_back' : myPokemons.type1 === 'Poison' ? 'poison_back' : myPokemons.type1 === 'Psychic' ? 'psychic_back' : myPokemons.type1 === 'Rock' ? 'rock_back' : myPokemons.type1 === 'Steel' ? 'steel_back' : ''">
+        :class="myPokemons.type1 === 'Grass' ? 'grass_back' : myPokemons.type1 === 'Fire' ? 'fire_back' : myPokemons.type1 === 'Water' ? 'water_back' : ''"
+        :style="`background-color: ${backColor}`">
         <div class="d-flex justify-content-center align-items-center mt-3">
             <img :src="myPokemons.imageUrl">
         </div>
--- src/components/data/index.js
+++ src/components/data/index.js
@@ -1,2 +1,23 @@
 
-const 
+const colorMap = {
+    "Bug": 'rgb(163, 231, 99)',
+    "Dark": 'rgb(77, 77, 77)',
+    "Dragon": 'rgb(117,93,225)',
+    "Electric": 'rgb(255, 255, 0)',
+    "Fairy": 'rgb(255, 192, 203)',
+    "Fighting": 'rgb(158, 76, 76)',
+    "Fire": '#FDDFDF',
+    "Flying": 'rgb(173, 216, 230)',
+    "Ghost": 'rgb(100, 0, 150)',
+    "Grass": '#DEFDE0',
+    "Ground": 'rgb(158, 123, 9)',
+    "Ice": 'rgb(106, 186, 212)',
+    "Normal": 'rgb(190, 190, 190)',
+    "Poison": 'rgb(128, 0, 128)',
+    "Psychic": 'rgb(201, 0, 184)',
+    "Rock": 'rgb(100, 50, 50)',
+    "Steel": 'rgb(148, 148, 148)',
+    "Water": '#DEF3FD'
+}
+
+export { colorMap };
